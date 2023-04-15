52992bb63800cf8bcd9eaaa35c77bf413ab9a304
yi,cong Song <handsd6111@gmail.com>
2023-04-15 10:55:10 +0000

    爬蟲欲開始前執行的工具包

---
+++ crawler/tools/install-command.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install python3-pip -y
+pip install selenium
+pip install pymysql
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt -f install -y
+sudo cp chromedriver.exe /usr/local/bin/chromedriver
+sudo chmod 755 /usr/local/bin/chromedriver
+google-chrome --version
+chromedriver
