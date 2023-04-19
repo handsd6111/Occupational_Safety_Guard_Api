0dfa57f23cd8d1312ada33040b38def3d3f5033b
yi,cong Song <handsd6111@gmail.com>
2023-04-19 17:02:15 +0000

    新增Swagger Info以及Notifying Agency的API說明檔案

---
+++ app/Docs/Info.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Docs;
+
+
+/**
+ * @OA\OpenApi(
+ *  @OA\Info(
+ *      title="Occupational Safety Guard Api",
+ *      version="1.0.0",
+ *      description="用於資料創新應用競賽-職場安全創新應用組的後端API",
+ *      @OA\Contact(
+ *          email="s20313116@stu.edu.tw"
+ *      )
+ *  ),
+ *  @OA\Server(
+ *      description="正式區",
+ *      url="https://cool.aronhome.com:51504"
+ *  ),
+ *  @OA\PathItem(
+ *      path="/"
+ *  )
+ * )
+ */
+interface Info
+{
+}
