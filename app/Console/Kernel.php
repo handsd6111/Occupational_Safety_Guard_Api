a0c1ebb2e57d67bfc24f751337bbe91e2599071a
yi,cong Song <handsd6111@gmail.com>
2023-08-05 12:28:36 +0000

    新增刪除過期的RefreshToken，並且進行系統排程。

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // 記得要去 crontab -e 寫入 * * * * * php /path/to/artisan schedule:run
+        $schedule->command('remove-if-refresh-token-expired')->daily();
     }
 
     /**
@@ -20,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
